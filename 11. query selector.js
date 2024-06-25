0ddfb984bc71a711e3ec63cdde41a05536ce2134
almashruf <tonoy5300@gmail.com>
2024-06-25 13:53:56 +0600

    new topic added

--- 11. query selector.js
+++ 11. query selector.js
@@ -1,1 +1,12 @@
 // eta diye amra getelementsbyid, by tagname, by classname eii 3 tar kaj ekta diyei amra kore felte parbo
+
+
+document.querySelector(".pid").innerHTML= "This is changed";
+
+// query selector in tagname op
+
+document.querySelector("p").innerHTML= "This is changed";
+
+//q selector in list tag 
+
+document.querySelector("li a").innerHTML="new text";
